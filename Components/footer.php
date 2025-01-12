2dd5719eb3202e833b0811a6fbdecd8222af3a43
Sariyet <sariyetmondalofficial@gmail.com>
2025-01-12 15:44:01 +0530

    Add files via upload

---
+++ Components/footer.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Footer</title>
+     <!--Font awesome cdn link-->
+     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
+     <!--custom css file link-->
+     <link rel="stylesheet" href="../css/User_style.css"> 
+</head>
+<body></body>
+
+<!--footer section starts-->
+<section class="footer">
+    <div class="box-container">
+        <div class="box">
+            <h3>quick links</h3>
+            <a href="#">home</a>
+            <a href="#">about</a>
+            <a href="#">products</a>
+            <a href="#">review</a>
+            <a href="#">contact</a>
+        </div>
+        <div class="box">
+            <h3>Extra links</h3>
+            <a href="#">my account</a>
+            <a href="#">my order</a>
+            <a href="#">my favorites</a>
+        </div>
+        <div class="box">
+            <h3>Locations</h3>
+            <a href="#">Mumbai</a>
+            <a href="#">Kolkata</a>
+            <a href="#">Delhi</a>
+            <a href="#">Chennai</a>
+        </div>
+
+        <div class="box">
+            <h3>contact info</h3>
+            <a href="#">+91 011 111011</a>
+            <a href="#">techtronix@official.com</a>
+            <a href="#">Kolkata, Kol- 700010</a>
+          <img src="../Images/payment.png" alt="">
+       
+        </div>
+    </div>
+    <div class="credit">
+        &copy; Created by <span> Techtronix Official</span> All Right Reserved,2024
+    </div>
+</section>
+
+<!--footer section ends-->
---
+++ Components/header.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Header</title>
+     <!--Font awesome cdn link-->
+     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
+     <!--custom css file link-->
+     <link rel="stylesheet" href="../css/User_style.css"> 
+</head>
+<body>
+
+    <!--Header section starts-->
+    <header>
+        <input type="checkbox" name="" id="toggler">
+        <label for="toggler" class="fas fa-bars"></label>
+        <a href="../Techtronix/index.php" class="logo"><img src="./Images/Techtronix-logo.png"></a>
+        <nav class="navbar">
+            <a href="../Techtronix/index.php">Home</a>
+            <a href="#about">About</a>
+            <a href="#products">Products</a>
+            <a href="#review">Review</a>
+            <a href="#contact">Contact</a>
+        </nav>
+        <div class="icons">
+            <a href="../Techtronix/User_pages/Wishlist.php" class="fas fa-heart"></a>
+            <a href="../Techtronix/User_pages/Cart.php" class="fas fa-shopping-cart"></a>
+            <a href="../Techtronix/Admin_pages/Admin_login.php" class="fas fa-user"></a>
+            
+        </div>
+    </header>
+     <!--Header section ends-->
+</body>
+</html>
