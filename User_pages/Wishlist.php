463ad1b6223cf3995707f877edff7b9441760d03
Sariyet <sariyetmondalofficial@gmail.com>
2025-01-12 15:49:29 +0530

    Add files via upload

---
+++ User_pages/Wishlist.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Responsive Shopping Cart design</title>
+	<link href="../CSS/Cart.css" rel="stylesheet">
+</head>
+<body>
+	<?php
+	@include "../Components/header.php";
+	?>
+    <div class="wrapper">
+		<h1>Wishlist</h1>
+		<div class="project">
+			<div class="cart-shop">
+				<div class="cart-box">
+					<img src="../Images/camera.jpeg">
+					<div class="content">
+						<h3>Adob camera FHD</h3>
+						<h4>Price: ₹19999</h4>
+						<p class="unit">Quantity: <input name="" value="2"></p>
+						<button class="btn-area"><i aria-hidden="true" class="fa fa-trash"></i> <span class="btn2">Remove</span></button>
+					</div>
+				</div>
+				<div class="cart-box">
+					<img src="../Images/drone.jpeg">
+					<div class="content">
+						<h3>Drone XF6</h3>
+						<h4>Price: ₹19875</h4>
+						<p class="unit">Quantity: <input name="" value="1"></p>
+						<button class="btn-area"><i aria-hidden="true" class="fa fa-trash"></i> <span class="btn2">Remove</span></button>
+					</div>
+				</div>
+				<div class="cart-box">
+					<img src="../Images/earphone.jpeg">
+					<div class="content">
+						<h3>Earphone C3</h3>
+						<h4>Price: ₹12899</h4>
+						<p class="unit">Quantity: <input name="" value="1"></p>
+						<button class="btn-area"><i aria-hidden="true" class="fa fa-trash"></i> <span class="btn2">Remove</span></button>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+<?php
+@include "../Components/footer.php";
+?>
+</body>
+</html>
